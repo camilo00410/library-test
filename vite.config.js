1a83cf546e3798f3eab7b827c2c83cebce86a1ea
Camilo loaiza <camiloandres00410@gmail.com>
2022-12-03 04:06:33 -0500

    bug with vite style

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,19 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+
+                'resources/login-assets/css/main.css',
+                'resources/login-assets/css/util.css',
+                'resources/login-assets/vendor/bootstrap/css/bootstrap.min.css',
+                'resources/login-assets/vendor/css-hamburgers/hamburgers.min.css',
+                'resources/login-assets/vendor/animate/animate.css',
+                'resources/login-assets/vendor/select2/select2.min.css',
+                'resources/login-assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+
+
+                'resources/login-assets/js/main.js',
+                'resources/login-assets/vendor/bootstrap/js/popper.js',
+                'resources/login-assets/vendor/bootstrap/js/bootstrap.min.js',
             ],
             refresh: true,
         }),
